126e3cccefd2551790224ae5ef5a3afb6950acd2
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2025-04-14 20:26:07 -0400

    feat: add intro message using variables and reassignment for partner info

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,19 @@
-// Write Your JavaScript Code Here
+// Initial variables with your info
+let personName = "Marcio";
+let pets = 1;
+let funFact = "I like to swim";
+
+// Logging your intro
+console.log(`My name is ${personName}.`);
+console.log(`I have ${pets} pet(s).`);
+console.log(`Fun fact: ${funFact}`);
+
+// Reassign with your partner's info
+personName = "Logan";
+pets = 2;
+funFact = "I love coding at McDonald's 😉";
+
+// Logging your partner's intro
+console.log(`My name is ${personName}.`);
+console.log(`I have ${pets} pet(s).`);
+console.log(`Fun fact: ${funFact}`);
